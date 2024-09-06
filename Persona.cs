e8808af268a6fcca89d6cf4372e9f08dc8050631
GianHB <gianhuaman79@gmail.com>
2024-09-06 16:30:09 -0500

    Constructor_Persona

---
+++ Persona.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proyecto
+{
+   public class Persona
+   {
+        public string nombre;
+        public int edad;
+
+        public Persona(string nombre, int edad)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+        }
+   }
+}
