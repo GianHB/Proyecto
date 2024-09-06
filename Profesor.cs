28e08e4b8a2f8113c3f8d654b69067118f8156cf
GianHB <gianhuaman79@gmail.com>
2024-09-06 16:34:22 -0500

    Herencia_Profesor

---
+++ Profesor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proyecto
+{
+   public class Profesor:Persona
+   {
+        public string especialidad;
+
+        public Profesor(string nombre, int edad, string especialidad) : base(nombre, edad)
+        {
+            this.especialidad = especialidad;
+        }
+   }
+}
